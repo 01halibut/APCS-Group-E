259862fc0d954debca67672281f15237b72fa942
Alan Li <alanpusongli@gmail.com>
2015-05-17 21:00:13 -0700

    Add flaps
    It flaps!

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,55 +1,122 @@
 package game;
+
+import resources.ResourceLoader;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class Main extends Application{
 	
-	private Button button = null;
-	private Group root = null;
+	final double ACC = 0.8;
 	private ImageView bkgrd = null ;
-	private Node flappy = null;
+	private ImageView flappy = null ;
+	private TranslateTransition flappyFall = null;
+	private TranslateTransition flappyFlap = null;
+	private Button button = null;
+	private MediaPlayer flap = null;
+	private Group root = null;
 	
-    private void addActionEventHandler(){
+    private void addButtonPressHandler(){
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	root.getChildren().remove(button);
+            	flappyFall = new TranslateTransition(
+            			new Duration(30 * Math.sqrt(2*(300 - flappy.getTranslateY())/ACC))
+            			, flappy);
+            	flappyFall.setToY(300);
+            	flappyFall.setInterpolator(new Interpolator() {
+            		protected double curve(double t) {
+    					return t * t;
+    				}});
+            	flappyFall.play();
+            }
+        });
     }
     
     private void addMouseEventHandler(){
-    }	
+    	root.onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            	flap.stop();
+            	if(root.getChildren().contains(button)) return;
+            	flappyFlap.setToY(flappy.getTranslateY() - 100);
+            	flappyFlap.setInterpolator(new Interpolator() {
+            		protected double curve(double t) {
+        				return Math.sqrt(t);
+        			}});
+            	addFlapFinishedHandler();
+            	flappyFlap.play();
+            	flap.play();
+            }
+        });
+    }
+    
+    private void addFlapFinishedHandler(){
+    	flappyFlap.setOnFinished(new EventHandler<ActionEvent>() {
+    		@Override
+    		public void handle(ActionEvent event) {
+            	flappyFall = new TranslateTransition(
+            			new Duration(30 * Math.sqrt(2*(300 - flappy.getTranslateY())/ACC))
+            			, flappy);
+            	flappyFall.setToY(300);
+            	flappyFall.setInterpolator(new Interpolator() {
+            		protected double curve(double t) {
+    					return t * t;
+    				}});
+            	flappyFall.play();
+            }
+    	});
+    }
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		//TODO 1: add background
-		bkgrd = new ImageView("background.png");
-
-		
-		
-		//TODO 2: add Flappy
-
-		
-		
-		//TODO 3: add Button
-
-		
-		
 		//Create a Group 
-		root = new Group( );
-		root.getChildren().add(bkgrd );
+		root = new Group();
 		
-		//TODO 4: add action handler to the button
-		addActionEventHandler();
+		bkgrd = new ImageView("./resources/background.png");
+		flappy = new ImageView("./resources/flappy.png");
+		button = new Button("Start");
+		
+		flap = new MediaPlayer(new Media(ResourceLoader.class.getResource("flap.mp3").toString()));
+		
+		flappyFlap = new TranslateTransition();
+    	flappyFlap = new TranslateTransition(new Duration(30 * Math.sqrt(200/ACC)), flappy);
+		flappyFall = new TranslateTransition();
 
-		//TODO 5: add mouse handler to the scene
+		
+		
+		//Add controls
+		root.getChildren().add( bkgrd );
+		root.getChildren().add( flappy );
+		root.getChildren().add( button );
+		
+		addButtonPressHandler();
 		addMouseEventHandler();
+		addFlapFinishedHandler();
 		
 		
 		//Create scene and add to stage
 		Scene scene = new Scene(root, 400, 400);
+		
+    	flappy.translateXProperty().set(scene.getWidth()/2 - 25);
+    	flappy.translateYProperty().set(scene.getHeight()/2 - 25);
+    	
+    	button.translateXProperty().set(scene.getWidth()/2 - 25);
+    	button.translateYProperty().set(scene.getHeight() - 50);
+    	
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
---
+++ src/resources/ResourceLoader.java
@@ -0,0 +1,7 @@
+package resources;
+
+public class ResourceLoader {
+	/*
+	 * This class is pretty much just here as a reference point
+	 */
+}
